793373e0e41b240ec8778d68442adb8ea32b9ab2
PengKang6 <907428301@qq.com>
2020-09-25 00:23:07 +0800

    wer指标计算

---
+++ hlp/stt/ds2/wer.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 21:33:40 2020
+
+@author: 彭康
+"""
+
+#输入的应该均是字符串的list?
+def wers(originals, results):
+    count = len(originals)
+    try:
+        assert count > 0
+    except:
+        print(originals)
+        raise("ERROR assert count>0 - looks like data is missing")
+    rates = []
+    mean = 0.0
+    assert count == len(results)
+    for i in range(count):
+        rate = wer(originals[i], results[i])
+        mean = mean + rate
+        rates.append(rate)
+    return rates, mean / float(count)
+
+def wer(original, result):
+    r"""
+    The WER is defined as the editing/Levenshtein distance on word level
+    divided by the amount of words in the original text.
+    In case of the original having more words (N) than the result and both
+    being totally different (all N words resulting in 1 edit operation each),
+    the WER will always be 1 (N / N = 1).
+    """
+    # The WER ist calculated on word (and NOT on character) level.
+    # Therefore we split the strings into words first:
+    original = original.split()
+    result = result.split()
+    return levenshtein(original, result) / float(len(original))
+
+def levenshtein(a,b):
+    "Calculates the Levenshtein distance between a and b."
+    n, m = len(a), len(b)
+    if n > m:
+        # Make sure n <= m, to use O(min(n,m)) space
+        a,b = b,a
+        n,m = m,n
+
+    current = list(range(n+1))
+    for i in range(1,m+1):
+        previous, current = current, [i]+[0]*n
+        for j in range(1,n+1):
+            add, delete = previous[j]+1, current[j-1]+1
+            change = previous[j-1]
+            if a[j-1] != b[i-1]:
+                change = change + 1
+            current[j] = min(add, delete, change)
+
+    return current[n]
