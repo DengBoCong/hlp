c1156e545b78a9ad3a8c78bdd9dc2c5f95eb66e8
mxx657845129 <657845129@qq.com>
2020-10-05 23:04:08 +0800

    tacotron2 model

--- hlp/tts/tacotron2/config/config.py
+++ hlp/tts/tacotron2/config/config.py
@@ -1,32 +1,26 @@
-from tacotron2.process.ljspeech import LJSPEECH_SYMBOLS as lj_symbols
 class Tacotron2Config(object):
     """Initialize Tacotron-2 Config."""
     def __init__(
         self,
-        dataset='ljspeech',
-        vocab_size=len(lj_symbols),
         embedding_hidden_size=512,
-        initializer_range=0.02,
-        layer_norm_eps=1e-6,
         embedding_dropout_prob=0.1,
-        n_speakers=5,
-        n_conv_encoder=3,
         encoder_conv_filters=512,
         encoder_conv_kernel_sizes=5,
-        encoder_conv_activation="mish",
         encoder_conv_dropout_rate=0.5,
         encoder_lstm_units=256,
-        reduction_factor=5,
+        decoder_dim=256,
+        decoder_lstm_dim=1024,
+        decoder_lstm_rate=0.1,
+
+        reduction_factor=1,
         n_prenet_layers=2,
         prenet_units=256,
-        prenet_activation="mish",
         prenet_dropout_rate=0.5,
-        n_lstm_decoder=1,
         decoder_lstm_units=1024,
         attention_type="lsa",
         attention_dim=128,
-        attention_filters=32,
-        attention_kernel=31,
+        attention_filters=64,
+        attention_kernel=5,
         n_mels=80,
         n_conv_postnet=5,
         postnet_conv_filters=512,
@@ -34,19 +28,10 @@ class Tacotron2Config(object):
         postnet_dropout_rate=0.1,
     ):
         """Init parameters for Tacotron-2 model."""
-        if dataset == "ljspeech":
-            self.vocab_size = vocab_size
-        else:
-            raise ValueError("No such dataset: {}".format(dataset))
         self.embedding_hidden_size = embedding_hidden_size
-        self.initializer_range = initializer_range
-        self.layer_norm_eps = layer_norm_eps
         self.embedding_dropout_prob = embedding_dropout_prob
-        self.n_speakers = n_speakers
-        self.n_conv_encoder = n_conv_encoder
         self.encoder_conv_filters = encoder_conv_filters
         self.encoder_conv_kernel_sizes = encoder_conv_kernel_sizes
-        self.encoder_conv_activation = encoder_conv_activation
         self.encoder_conv_dropout_rate = encoder_conv_dropout_rate
         self.encoder_lstm_units = encoder_lstm_units
 
@@ -54,15 +39,16 @@ class Tacotron2Config(object):
         self.reduction_factor = reduction_factor
         self.n_prenet_layers = n_prenet_layers
         self.prenet_units = prenet_units
-        self.prenet_activation = prenet_activation
         self.prenet_dropout_rate = prenet_dropout_rate
-        self.n_lstm_decoder = n_lstm_decoder
         self.decoder_lstm_units = decoder_lstm_units
         self.attention_type = attention_type
         self.attention_dim = attention_dim
         self.attention_filters = attention_filters
         self.attention_kernel = attention_kernel
         self.n_mels = n_mels
+        self.decoder_dim=decoder_dim
+        self.decoder_lstm_dim=decoder_lstm_dim
+        self.decoder_lstm_rate=decoder_lstm_rate
 
         # postnet
         self.n_conv_postnet = n_conv_postnet
