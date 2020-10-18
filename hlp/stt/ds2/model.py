c305134f0d29d5ca71a276bfa0bf93045a3dd3bc
hlp-ai <hlp_ai@sina.com>
2020-10-18 17:23:39 +0800

    对DS2包进行reformat code。

--- hlp/stt/ds2/model.py
+++ hlp/stt/ds2/model.py
@@ -4,63 +4,15 @@ Created on Tue Sep 15 16:50:12 2020
 @author: 彭康
 """
 
-#模型搭建
-#step1：1-3 Conv1D -> 1BN -> 1-3 bi_gru -> 1BN -> 1dense
-import tensorflow as tf
 import config
+# 模型搭建
+# step1：1-3 Conv1D -> 1BN -> 1-3 bi_gru -> 1BN -> 1dense
+import tensorflow as tf
 from utils import get_index_and_char_map
 
 
-#函数式构建DS2模型
+# 函数式构建DS2模型
 def DS2_func(
-    n_mfcc=config.configs_other()["n_mfcc"],
-    conv_layers=config.configs_model()["conv_layers"],
-    filters=config.configs_model()["conv_filters"],
-    kernel_size=config.configs_model()["conv_kernel_size"],
-    strides=config.configs_model()["conv_strides"],
-    bi_gru_layers=config.configs_model()["bi_gru_layers"],
-    gru_units=config.configs_model()["gru_units"],
-    dense_units=len(get_index_and_char_map()[0])+2
-    ):
-    inputs=tf.keras.Input(shape=(None,None,n_mfcc))
-    x=inputs
-    for _ in range(conv_layers):
-        x = tf.keras.layers.Conv1D(
-                filters=filters,
-                name="conv{}".format(_+1),
-                kernel_size=kernel_size,
-                padding="same",
-                activation="relu",
-                strides=strides
-                )(x)
-    x=tf.keras.layers.BatchNormalization(
-                axis=-1,
-                momentum=0.99,
-                epsilon=0.001
-                )(x)
-    for _ in range(bi_gru_layers):
-        x = tf.keras.layers.Bidirectional(
-                tf.keras.layers.GRU(
-                        gru_units,
-                        name="bi_gru{}".format(_+1),
-                        return_sequences=True,
-                        activation="relu"
-                        ),
-                merge_mode="sum"
-                )(x)
-    x=tf.keras.layers.BatchNormalization(
-                axis=-1,
-                momentum=0.99,
-                epsilon=0.001
-                )(x)
-    outputs=tf.keras.layers.Dense(dense_units,activation="softmax")(x)
-    return tf.keras.Model(inputs,outputs)
-
-#子类化构建DS2模型
-class DS2(tf.keras.Model):
-    #dense_units=num_classes
-    def __init__(
-        self,
         n_mfcc=config.configs_other()["n_mfcc"],
         conv_layers=config.configs_model()["conv_layers"],
         filters=config.configs_model()["conv_filters"],
@@ -68,36 +20,85 @@ class DS2(tf.keras.Model):
         strides=config.configs_model()["conv_strides"],
         bi_gru_layers=config.configs_model()["bi_gru_layers"],
         gru_units=config.configs_model()["gru_units"],
-        dense_units=len(get_index_and_char_map()[0])+2
-        ):
-        super(DS2,self).__init__()
-        self.conv_layers=conv_layers
+        dense_units=len(get_index_and_char_map()[0]) + 2
+):
+    inputs = tf.keras.Input(shape=(None, None, n_mfcc))
+    x = inputs
+    for _ in range(conv_layers):
+        x = tf.keras.layers.Conv1D(
+            filters=filters,
+            name="conv{}".format(_ + 1),
+            kernel_size=kernel_size,
+            padding="same",
+            activation="relu",
+            strides=strides
+        )(x)
+    x = tf.keras.layers.BatchNormalization(
+        axis=-1,
+        momentum=0.99,
+        epsilon=0.001
+    )(x)
+    for _ in range(bi_gru_layers):
+        x = tf.keras.layers.Bidirectional(
+            tf.keras.layers.GRU(
+                gru_units,
+                name="bi_gru{}".format(_ + 1),
+                return_sequences=True,
+                activation="relu"
+            ),
+            merge_mode="sum"
+        )(x)
+    x = tf.keras.layers.BatchNormalization(
+        axis=-1,
+        momentum=0.99,
+        epsilon=0.001
+    )(x)
+    outputs = tf.keras.layers.Dense(dense_units, activation="softmax")(x)
+    return tf.keras.Model(inputs, outputs)
+
+
+# 子类化构建DS2模型
+class DS2(tf.keras.Model):
+    # dense_units=num_classes
+    def __init__(
+            self,
+            n_mfcc=config.configs_other()["n_mfcc"],
+            conv_layers=config.configs_model()["conv_layers"],
+            filters=config.configs_model()["conv_filters"],
+            kernel_size=config.configs_model()["conv_kernel_size"],
+            strides=config.configs_model()["conv_strides"],
+            bi_gru_layers=config.configs_model()["bi_gru_layers"],
+            gru_units=config.configs_model()["gru_units"],
+            dense_units=len(get_index_and_char_map()[0]) + 2
+    ):
+        super(DS2, self).__init__()
+        self.conv_layers = conv_layers
         self.conv = tf.keras.layers.Conv1D(
-                filters=filters,
-                kernel_size=kernel_size,
-                strides=strides,
-                padding="same",
-                activation="relu",
-                input_shape=(None,None,n_mfcc)
-                )
-        self.bi_gru_layers=bi_gru_layers
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding="same",
+            activation="relu",
+            input_shape=(None, None, n_mfcc)
+        )
+        self.bi_gru_layers = bi_gru_layers
         self.bi_gru = tf.keras.layers.Bidirectional(
-                tf.keras.layers.GRU(
-                        gru_units,
-                        activation="relu",
-                        return_sequences=True
-                        ),
-                merge_mode="sum"
-                )
+            tf.keras.layers.GRU(
+                gru_units,
+                activation="relu",
+                return_sequences=True
+            ),
+            merge_mode="sum"
+        )
         self.bn = tf.keras.layers.BatchNormalization(
-                axis=-1,
-                momentum=0.99,
-                epsilon=0.001
-                )
-        self.ds = tf.keras.layers.Dense(dense_units,activation="softmax")
-    
-    def call(self,inputs):
-        x=inputs
+            axis=-1,
+            momentum=0.99,
+            epsilon=0.001
+        )
+        self.ds = tf.keras.layers.Dense(dense_units, activation="softmax")
+
+    def call(self, inputs):
+        x = inputs
         for _ in range(self.conv_layers):
             x = self.conv(x)
         x = self.bn(x)
@@ -106,6 +107,7 @@ class DS2(tf.keras.Model):
         x = self.bn(x)
         x = self.ds(x)
         return x
+
 
 if __name__ == "__main__":
     pass
--- hlp/stt/ds2/utils.py
+++ hlp/stt/ds2/utils.py
@@ -5,35 +5,34 @@ Created on Wed Sep 16 21:15:16 2020
 @author: 彭康
 """
 
-import os
-import time
 import wave
-import librosa
-import numpy as np
-import pyaudio
-import tensorflow as tf
+
 import config
+import librosa
+import pyaudio
 
 
-#基于数据文本规则的行获取
+# 基于数据文本规则的行获取
 def text_process(str):
     """
     #文本每行"string\n"
     return str.strip().lower()
     """
-    #当前数据文本的每行为'index string\n'
-    return str.strip().split(" ",1)[1].lower()
+    # 当前数据文本的每行为'index string\n'
+    return str.strip().split(" ", 1)[1].lower()
 
-#音频的处理
-def wav_to_mfcc(n_mfcc,wav_path):
-    #加载音频
-    y, sr = librosa.load(wav_path,sr=None)
-    #提取mfcc(返回list(timestep,n_mfcc))
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc).transpose(1,0).tolist()
+
+# 音频的处理
+def wav_to_mfcc(n_mfcc, wav_path):
+    # 加载音频
+    y, sr = librosa.load(wav_path, sr=None)
+    # 提取mfcc(返回list(timestep,n_mfcc))
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc).transpose(1, 0).tolist()
     return mfcc
 
-#字符序列list转成数字label list
-def text_to_int_sequence(text,char_map):
+
+# 字符序列list转成数字label list
+def text_to_int_sequence(text, char_map):
     int_sequence = []
     for ch in text:
         if ch == ' ':
@@ -43,26 +42,28 @@ def text_to_int_sequence(text,char_map):
         int_sequence.append(i)
     return int_sequence
 
-#数字label list转成字符序列list
-def int_to_text_sequence(seq,index_map):
+
+# 数字label list转成字符序列list
+def int_to_text_sequence(seq, index_map):
     text_sequence = []
     for i in seq:
-        if i>=1 and i<=(len(index_map)):
+        if i >= 1 and i <= (len(index_map)):
             ch = index_map[i]
         else:
             ch = ''
         text_sequence.append(ch)
-    #"".join(text_sequence)就会转成字符串
+    # "".join(text_sequence)就会转成字符串
     return text_sequence
-        
-#输入的两个参数均是字符串的list,是wer计算的入口
+
+
+# 输入的两个参数均是字符串的list,是wer计算的入口
 def wers(originals, results):
     count = len(originals)
     try:
         assert count > 0
     except:
         print(originals)
-        raise("ERROR assert count>0 - looks like data is missing")
+        raise ("ERROR assert count>0 - looks like data is missing")
     rates = []
     mean = 0.0
     assert count == len(results)
@@ -71,6 +72,7 @@ def wers(originals, results):
         mean = mean + rate
         rates.append(rate)
     return rates, mean / float(count)
+
 
 def _wer(original, result):
     r"""
@@ -85,6 +87,7 @@ def _wer(original, result):
     original = original.split()
     result = result.split()
     return _levenshtein(original, result) / float(len(original))
+
 
 def lers(originals, results):
     count = len(originals)
@@ -107,69 +110,72 @@ def lers(originals, results):
         rates.append(rate)
         norm_rates.append(round(normrate, 4))
 
-    return rates, (mean / float(count)), norm_rates, (norm_mean/float(count))
+    return rates, (mean / float(count)), norm_rates, (norm_mean / float(count))
 
-def _levenshtein(a,b):
+
+def _levenshtein(a, b):
     "Calculates the Levenshtein distance between a and b."
     n, m = len(a), len(b)
     if n > m:
         # Make sure n <= m, to use O(min(n,m)) space
-        a,b = b,a
-        n,m = m,n
+        a, b = b, a
+        n, m = m, n
 
-    current = list(range(n+1))
-    for i in range(1,m+1):
-        previous, current = current, [i]+[0]*n
-        for j in range(1,n+1):
-            add, delete = previous[j]+1, current[j-1]+1
-            change = previous[j-1]
-            if a[j-1] != b[i-1]:
+    current = list(range(n + 1))
+    for i in range(1, m + 1):
+        previous, current = current, [i] + [0] * n
+        for j in range(1, n + 1):
+            add, delete = previous[j] + 1, current[j - 1] + 1
+            change = previous[j - 1]
+            if a[j - 1] != b[i - 1]:
                 change = change + 1
             current[j] = min(add, delete, change)
 
     return current[n]
 
-#获取麦克风录音并保存在filepath中
+
+# 获取麦克风录音并保存在filepath中
 def record(file_path):
-        CHUNK = 256
-        FORMAT = pyaudio.paInt16
-        CHANNELS = 1                # 声道数
-        RATE = 16000               # 采样率
-        RECORD_SECONDS = config.configs_record()["record_times"]        #录音时长
-        WAVE_OUTPUT_FILENAME = file_path
-        p = pyaudio.PyAudio()
+    CHUNK = 256
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1  # 声道数
+    RATE = 16000  # 采样率
+    RECORD_SECONDS = config.configs_record()["record_times"]  # 录音时长
+    WAVE_OUTPUT_FILENAME = file_path
+    p = pyaudio.PyAudio()
 
-        stream = p.open(format=FORMAT,
-                        channels=CHANNELS,
-                        rate=RATE,
-                        input=True,
-                        frames_per_buffer=CHUNK)
-        print("开始录音：请在%d秒内输入语音:" % (RECORD_SECONDS))
-        frames = []
-        for i in range(1,int(RATE / CHUNK * RECORD_SECONDS)+1):
-            data = stream.read(CHUNK)
-            frames.append(data)
-            if (i % (RATE / CHUNK)) == 0:
-                print('\r%s%d%s' % ("剩余",int(RECORD_SECONDS-(i//(RATE / CHUNK))),"秒"),end="")
-        print("\n录音结束\n")
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    print("开始录音：请在%d秒内输入语音:" % (RECORD_SECONDS))
+    frames = []
+    for i in range(1, int(RATE / CHUNK * RECORD_SECONDS) + 1):
+        data = stream.read(CHUNK)
+        frames.append(data)
+        if (i % (RATE / CHUNK)) == 0:
+            print('\r%s%d%s' % ("剩余", int(RECORD_SECONDS - (i // (RATE / CHUNK))), "秒"), end="")
+    print("\n录音结束\n")
 
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(p.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        wf.writeframes(b''.join(frames))
-        wf.close()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 
-#从char_set.txt里边构建index_map和char_map
+
+# 从char_set.txt里边构建index_map和char_map
 def get_index_and_char_map():
     char_set_path = config.configs_other()["char_set_path"]
     index_map = {}
     char_map = {}
-    with open(char_set_path,"r") as f:
+    with open(char_set_path, "r") as f:
         map_list = f.readlines()
     for i in range(len(map_list)):
         ci = map_list[i].strip().split()
@@ -178,7 +184,7 @@ def get_index_and_char_map():
             index_map[int(ci[1])] = " "
         else:
             index_map[int(ci[1])] = ci[0]
-    return index_map,char_map
+    return index_map, char_map
 
 
 if __name__ == "__main__":
