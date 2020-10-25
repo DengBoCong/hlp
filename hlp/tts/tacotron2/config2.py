47a0974c453333f1daeab9ac549cae2f4558ff34
mxx657845129 <657845129@qq.com>
2020-10-25 15:45:34 +0800

    解决冲突问题

--- hlp/tts/tacotron2/config2.py
+++ hlp/tts/tacotron2/config2.py
@@ -1,59 +1,72 @@
 class Tacotron2Config(object):
     """Initialize Tacotron-2 Config."""
-
     def __init__(
-            self,
-            dataset='ljspeech',
-            # vocab_size=len(lj_symbols),
-            embedding_hidden_size=512,
-            initializer_range=0.02,
-            layer_norm_eps=1e-6,
-            embedding_dropout_prob=0.1,
-            n_speakers=5,
-            n_conv_encoder=3,
-            encoder_conv_filters=512,
-            encoder_conv_kernel_sizes=5,
-            encoder_conv_activation="mish",
-            encoder_conv_dropout_rate=0.5,
-            encoder_lstm_units=256,
-            decoder_dim=256,
-            decoder_lstm_dim=1024,
-            decoder_lstm_rate=0.1,
+        self,
+        vocab_size=5,
+        embedding_hidden_size=512,
+        initializer_range=0.02,
+        layer_norm_eps=1e-6,
+        embedding_dropout_prob=0.1,
+        n_speakers=5,
+        n_conv_encoder=3,
+        encoder_conv_filters=512,
+        encoder_conv_kernel_sizes=5,
+        encoder_conv_activation="mish",
+        encoder_conv_dropout_rate=0.5,
+        encoder_lstm_units=256,
+        decoder_dim=256,
+        decoder_lstm_dim=1024,
+        decoder_lstm_rate=0.1,
 
-            # Attention parameters
-            attention_dim=128,
+        # Attention parameters
+        attention_dim=128,
 
-            # Location Layer parameters
-            attention_filters=56,
-            attention_kernel=31,
+        # Location Layer parameters
+        attention_filters=32,
+        attention_kernel=31,
 
-            # Mel-post processing network parameters
-            # postnet_embedding_dim=512,
-            # postnet_kernel_size=5,
-            # postnet_n_convolutions=5,
+        # Mel-post processing network parameters
+        # postnet_embedding_dim=512,
+        # postnet_kernel_size=5,
+        # postnet_n_convolutions=5,
 
-            reduction_factor=5,
-            n_prenet_layers=2,
-            prenet_units=256,
-            prenet_activation="mish",
-            prenet_dropout_rate=0.5,
-            n_lstm_decoder=1,
-            decoder_lstm_units=1024,
-            attention_type="lsa",
 
-            gate_threshold=0.5,
-            n_mels=80,
-            n_conv_postnet=5,
-            postnet_conv_filters=512,
-            postnet_conv_kernel_sizes=5,
-            postnet_dropout_rate=0.1,
-            # path
-            text_train_path=r".\text\text_train\wenzi.txt",
-            wave_train_path=r"./wavs/wave_train/",
-            text_test_path=r".\text\text_test\wenzi.txt",
-            wave_test_path=r"./wavs/wave_test/",
-            # 一些参数
-            batch_size=2
+        reduction_factor=5,
+        n_prenet_layers=2,
+        prenet_units=256,
+        prenet_activation="mish",
+        prenet_dropout_rate=0.5,
+        n_lstm_decoder=1,
+        decoder_lstm_units=1024,
+        attention_type="lsa",
+
+        gate_threshold=0.5,
+        n_conv_postnet=5,
+        postnet_conv_filters=512,
+        postnet_conv_kernel_sizes=5,
+        postnet_dropout_rate=0.1,
+        #path
+        text_train_path=r"./number/train/text/wenzi.txt",
+        wave_train_path= r"./number/train/wave/",
+        #text_test_path= r".\text\text_test\wenzi.txt",
+        #wave_test_path = r"./wavs/wave_test/",
+        #关于音频的参数
+        sr=22050,# Sample rate.
+        n_fft = 2048,  # fft points (samples)
+        frame_shift = 0.0125,  # seconds
+        frame_length = 0.05,  # seconds
+        #hop_length = int(sr*frame_shift),  # samples.
+        hop_length=275,
+        #win_length = int(sr*frame_length),  # samples.
+        win_length = 1102,
+        n_mels = 80,  # Number of Mel banks to generate
+        power = 1.2,  # Exponent for amplifying the predicted magnitude
+        n_iter = 100,  # Number of inversion iterations
+        preemphasis = .97,  # or None
+        max_db = 100,
+        ref_db = 20,
+        top_db = 15,
+        batch_size=2
 
     ):
         """Init parameters for Tacotron-2 model."""
@@ -68,6 +81,7 @@ class Tacotron2Config(object):
         self.encoder_conv_activation = encoder_conv_activation
         self.encoder_conv_dropout_rate = encoder_conv_dropout_rate
         self.encoder_lstm_units = encoder_lstm_units
+        self.vocab_size=vocab_size
 
         # decoder param
         self.reduction_factor = reduction_factor
@@ -82,9 +96,9 @@ class Tacotron2Config(object):
         self.attention_filters = attention_filters
         self.attention_kernel = attention_kernel
         self.n_mels = n_mels
-        self.decoder_dim = decoder_dim
-        self.decoder_lstm_dim = decoder_lstm_dim
-        self.decoder_lstm_rate = decoder_lstm_rate
+        self.decoder_dim=decoder_dim
+        self.decoder_lstm_dim=decoder_lstm_dim
+        self.decoder_lstm_rate=decoder_lstm_rate
         self.gate_threshold = gate_threshold
         # postnet
         self.n_conv_postnet = n_conv_postnet
@@ -92,18 +106,23 @@ class Tacotron2Config(object):
         self.postnet_conv_kernel_sizes = postnet_conv_kernel_sizes
         self.postnet_dropout_rate = postnet_dropout_rate
         # path
-        self.text_train_path = text_train_path
+        self.text_train_path=text_train_path
         self.wave_train_path = wave_train_path
-        self.text_test_path = text_test_path
-        self.wave_test_path = wave_test_path
-        self.batch_size = batch_size
+#        self.text_test_path = text_test_path
+ #       self.wave_test_path = wave_test_path
+        self.batch_size=batch_size
+        # 声音参数
+        self.sr=sr
+        self.n_fft=n_fft
+        self.frame_shift=frame_shift
+        self.frame_length=frame_length
+        self.hop_length=hop_length
+        self.win_length=win_length
+        self.n_mels=n_mels
+        self.power=power
+        self.n_iter=n_iter
+        self.preemphasis=preemphasis
+        self.max_db=max_db
+        self.ref_db=ref_db
+        self.top_db=top_db
 
-
-"""----------------------checkpoint--------------------------"""
-
-
-def configs_checkpoint():
-    configs_checkpoint = {}
-    configs_checkpoint["directory"] = "./checkpoint"  # 检查点保存路径
-    configs_checkpoint["max_to_keep"] = 5  # 保存的最近的检查点的最多个数
-    return configs_checkpoint
--- hlp/tts/tacotron2/train.py
+++ hlp/tts/tacotron2/train.py
@@ -1,50 +1,41 @@
 import os
 import time
-
 import tensorflow as tf
+from prepocesses import Dataset_txt,Dataset_wave,create_dataset
 from config2 import Tacotron2Config
 from model.tacotron2 import Tacotron2
-from utils import process_text, process_wave, tf_data
-
-
-# 数据处理部分
-# 对文字的处理
-# path_to_file = r".\text\wenzi.txt"
-
-
-# 损失函数和优化器
+from griffinlim.griffinlim import melspectrogram2wav,plot_spectrogram_to_numpy
+import scipy.io.wavfile as wave
+import matplotlib.pyplot as plt
+#损失函数和优化器
 def loss_function(mel_out, mel_out_postnet, mel_gts):
-    mel_gts = tf.transpose(mel_gts, [0, 2, 1])
-    mel_loss = tf.keras.losses.MeanSquaredError()(mel_out, mel_gts) + tf.keras.losses.MeanSquaredError()(
-        mel_out_postnet, mel_gts)
+    mel_gts=tf.transpose(mel_gts,[0,2,1])
+    mel_loss = tf.keras.losses.MeanSquaredError()(mel_out, mel_gts) + tf.keras.losses.MeanSquaredError()(mel_out_postnet, mel_gts)
     return mel_loss
 
-
-# 单次训练
-def train_step(input_ids, mel_gts, model, optimizer):
+#单次训练
+def train_step(input_ids, mel_gts,model,optimizer):
     loss = 0
     with tf.GradientTape() as tape:
-        mel_outputs, mel_outputs_postnet, gate_outputs, alignments = model(input_ids, mel_gts)
-        loss += loss_function(mel_outputs, mel_outputs_postnet, mel_gts)
-    # batch_loss = (loss / int(mel_gts.shape[1]))
-    batch_loss = loss * 10000
+        mel_outputs, mel_outputs_postnet, gate_outputs, alignments=model(input_ids,mel_gts)
+        loss+=loss_function(mel_outputs,mel_outputs_postnet,mel_gts)
+    #batch_loss = (loss / int(mel_gts.shape[1]))
+    batch_loss = loss
     variables = model.trainable_variables
     gradients = tape.gradient(loss, variables)  # 计算损失对参数的梯度
     optimizer.apply_gradients(zip(gradients, variables))  # 优化器反向传播更新参数
-    return batch_loss, mel_outputs_postnet
+    return batch_loss,mel_outputs_postnet
 
-
-# 启动训练
-def train(model, optimizer, dataset, epochs, steps_per_epoch):
+#启动训练
+def train(model, optimizer,dataset, epochs,steps_per_epoch):
     for epoch in range(epochs):
         start = time.time()
-
         total_loss = 0
         print("次数：+1")
         for (batch, (input_ids, mel_gts)) in enumerate(dataset.take(steps_per_epoch)):
             batch_start = time.time()
-            print(input_ids.shape)
-            batch_loss, mel_outputs = train_step(input_ids, mel_gts, model, optimizer)  # 训练一个批次，返回批损失
+            #print("input_ids",input_ids.shape)
+            batch_loss, mel_outputs = train_step(input_ids, mel_gts,model, optimizer)  # 训练一个批次，返回批损失
             total_loss += batch_loss
 
             if batch % 2 == 0:
@@ -54,42 +45,55 @@ def train(model, optimizer, dataset, epochs, steps_per_epoch):
                 print('Time taken for 2 batches {} sec\n'.format(time.time() - batch_start))
 
         # 每 2 个周期（epoch），保存（检查点）一次模型
-        if (epoch + 1) % 20 == 0:
-            checkpoint.save(file_prefix=checkpoint_prefix)
+        # if (epoch + 1) % 20 == 0:
+        #     checkpoint.save(file_prefix=checkpoint_prefix)
 
         print('Epoch {} Loss {:.4f}'.format(epoch + 1,
                                             total_loss / steps_per_epoch))
         print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+    return mel_outputs
 
-
-# 画频谱图
-
-if __name__ == "__main__":
-    config = Tacotron2Config(n_speakers=1, reduction_factor=1)
-    batch_size = config.batch_size
-    # 设置文件路径
-    text_train_path = config.text_train_path
+#画频谱图
+if __name__=="__main__":
+    config = Tacotron2Config()
+    batch_size=config.batch_size
+    #设置文件路径
+    text_train_path=config.text_train_path
     wave_train_path = config.wave_train_path
-    # 取数据
-    input_ids, vocab_inp_size = process_text(text_train_path)
-    print("input_ids:", input_ids.shape)
-    print("vocab_inp_size::::::::::", vocab_inp_size)
-    mel_gts = process_wave(wave_train_path)
-    # 建立输入输出流
-    dataset, steps_per_epoch = tf_data(batch_size, input_ids, mel_gts)
+    #取数据
+    input_ids,en_tokenizer=Dataset_txt(text_train_path)
+    input_ids=tf.convert_to_tensor(input_ids)
+    print("input_ids:",input_ids.shape)
+    mel_gts=Dataset_wave(wave_train_path)
+    print("mel_gts:", mel_gts.shape)
+    #建立输入输出流
+    dataset,steps_per_epoch=create_dataset(batch_size, input_ids, mel_gts)
     # 初始化模型
+    vocab_inp_size=config.vocab_size
     tacotron2 = Tacotron2(vocab_inp_size, config)
     optimizer = tf.keras.optimizers.Adam(lr=0.001)
-    epochs = 2
-    # 训练
-    train(tacotron2, optimizer, dataset, epochs, steps_per_epoch)
-    # 检查点
-    checkpoint_dir = './training_checkpoints2'
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-    checkpoint = tf.train.Checkpoint(tacotron2=tacotron2)
-    manager = tf.train.CheckpointManager(
-        checkpoint,
-        directory='./training_checkpoints2',
-        max_to_keep=5
-    )
-    manager.save()
+    epochs=10
+    #训练
+    mel_outputs=train(tacotron2, optimizer, dataset, epochs, steps_per_epoch)
+    print("mel_outputs:",mel_outputs.shape)
+    wav=melspectrogram2wav(mel_outputs[1].numpy())
+    sr=22050
+    #wave.write('4.wav', rate=sr, data=wav)
+    print(mel_outputs[1])
+    plt.figure()
+    plt.subplot(3, 1, 1)
+    plt.imshow(plot_spectrogram_to_numpy(mel_outputs[1].numpy()))
+    plt.show()
+    # # 检查点
+    #
+    # checkpoint_dir = './training_checkpoints2'
+    # checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    # checkpoint = tf.train.Checkpoint(tacotron2=tacotron2)
+    # manager = tf.train.CheckpointManager(
+    #     checkpoint,
+    #     directory='./training_checkpoints2',
+    #     max_to_keep=5
+    #     )
+    # manager.save()
+
+
