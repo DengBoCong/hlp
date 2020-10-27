60c7285cff648d2aa8175d965fc0660f28ac511c
PengKang6 <907428301@qq.com>
2020-10-27 23:49:45 +0800

    使用generator

--- hlp/stt/ds2/evaluate.py
+++ hlp/stt/ds2/evaluate.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from model import get_ds2_model, decode_output
+from load_dataset import load_data
+from metric import lers, wers
+from model import decode_output, get_ds2_model
 from utils import get_config, get_index_word
-from data_preprocess import load_data
-from metric import wers, lers
 
 if __name__ == "__main__":
     configs = get_config()
@@ -18,35 +18,42 @@ if __name__ == "__main__":
     if manager.latest_checkpoint:
         checkpoint.restore(manager.latest_checkpoint)
 
-    
     test_data_path = configs["test"]["data_path"]
     num_examples = configs["test"]["num_examples"]
-    #加载测试集数据并通过模型计算出结果序列
-    inputs, labels_list = load_data(test_data_path, "test", num_examples)
-    originals = labels_list
-    results = []
-    y_pred = model(inputs)
-    output = tf.keras.backend.ctc_decode(
-        y_pred=y_pred,
-        input_length=tf.fill([y_pred.shape[0]], y_pred.shape[1]),
-        greedy=True
-    )
-    results_int_list = output[0][0].numpy().tolist()
-
-    # 构建字符集对象,并解码出预测的结果list
-    index_word = get_index_word()
-    for i in range(len(results_int_list)):
-        str = decode_output(results_int_list[i], index_word).strip()
-        results.append(str)
     
-    # 通过wer、ler指标评价模型
-    rates_wers, aver_wers = wers(originals, results)
-    rates_lers, aver_lers, norm_rates_lers, norm_aver_lers = lers(originals, results)
+    #加载测试集数据生成器
+    test_data_generator = load_data(test_data_path, "test", num_examples)
+
+    aver_wers = 0
+    aver_lers = 0
+    aver_norm_lers = 0
+    
+    for batchs, (input_tensor, labels_list) in test_data_generator:
+        originals = labels_list
+        results = []
+        y_pred = model(input_tensor)
+        output = tf.keras.backend.ctc_decode(
+            y_pred=y_pred,
+            input_length=tf.fill([y_pred.shape[0]], y_pred.shape[1]),
+            greedy=True
+        )
+        results_int_list = output[0][0].numpy().tolist()
+
+        # 构建字符集对象,并解码出预测的结果list
+        index_word = get_index_word()
+        for i in range(len(results_int_list)):
+            str = decode_output(results_int_list[i], index_word).strip()
+            results.append(str)
+        
+        # 通过wer、ler指标评价模型
+        rates_wer, aver_wer = wers(originals, results)
+        rates_ler, aver_ler, norm_rates_ler, norm_aver_ler = lers(originals, results)
+        aver_wers += aver_wer
+        aver_lers += aver_ler
+        aver_norm_lers += norm_aver_ler
+    
     print("WER:")
-    print("rates", rates_wers)
-    print("aver:", aver_wers)
+    print("aver:", aver_wers/batchs)
     print("LER:")
-    print("rates:", rates_lers)
-    print("aver:", aver_lers)
-    print("norm_rates:", norm_rates_lers)
-    print("norm_aver:", norm_aver_lers)
+    print("aver:", aver_lers/batchs)
+    print("aver_norm:", aver_norm_lers/batchs)
--- hlp/stt/ds2/text_process.py
+++ hlp/stt/ds2/text_process.py
@@ -1,6 +1,7 @@
 import re
 import tensorflow as tf
 from utils import get_config
+import tensorflow as tf
 
 
 #此方法依据文本是中文文本还是英文文本，若为英文文本是按字符切分还是按单词切分
@@ -8,27 +9,85 @@ def preprocess_sentence(str):
     configs = get_config()
     mode = configs["preprocess"]["text_process_mode"]
     if mode == "cn":
-        return preprocess_ch_sentence(str)
+        return preprocess_sentence_ch(str)
     elif mode == "en_word":
-        return preprocess_en_sentence_word(str)
+        return preprocess_sentence_en_word(str)
     else:
-        return preprocess_en_sentence_char(str)
+        return preprocess_sentence_en_char(str)
 
 #基于数据文本规则的行获取
 def text_row_process(str):
     configs = get_config()
     style = configs["preprocess"]["text_raw_style"]
     if style == 1:
-        #当前数据文本的每行为'index string\n',且依据英文单词切分
+        #当前数据文本的每行为'index string\n'
         return str.strip().split(" ",1)[1].lower()
+    elif style == 2:
+        #当前数据文本的每行为'index\tstring\n'
+        return str.strip().split("\t",1)[1].lower()
     else:
         #文本每行"string\n"
         return str.strip().lower()
 
-# 对英文句子：小写化，切分句子，添加开始和结束标记，按单词切分
-def preprocess_en_sentence_word(s):
-    s = s.lower().strip()
+#word_list转token序列
+def text_to_int_sequence(word_list, word_index):
+    int_sequence = []
+    for c in word_list:
+        int_sequence.append(int(word_index[c]))
+    return int_sequence
 
+#基于每个生成器构建模型所需的文本数据
+def get_text_label(data_path, text_data_path, i, batch_size, word_index):
+    configs = get_config()
+    max_targets_len = configs["preprocess"]["max_targets_len"]
+    
+    target_list = []
+    target_length_list = []
+    with open(data_path + "/" + text_data_path, "r") as f:
+        sentence_list = f.readlines()
+    for sentence in sentence_list[i*batch_size : (i+1)*batch_size]:
+        sen_word_list = preprocess_sentence(text_row_process(sentence)).split(" ")
+        sen_int_list = text_to_int_sequence(sen_word_list, word_index)
+        target_length_list.append([len(sen_word_list)])
+        target_list.append(sen_int_list)
+    
+    target_tensor = tf.keras.preprocessing.sequence.pad_sequences(
+        target_list,
+        maxlen=max_targets_len,
+        padding='post'
+        )
+    target_length = tf.convert_to_tensor(target_length_list)
+    return target_tensor, target_length
+
+#获取测试时所需的文本list
+def get_lables_list(data_path, text_data_path, i, batch_size):
+    labels_list = []
+    with open(data_path + "/" + text_data_path, "r") as f:
+        sentence_list = f.readlines()
+    for sentence in sentence_list[i*batch_size : (i+1)*batch_size]:
+        labels_list.append(text_row_process(sentence))
+    return labels_list
+
+#预处理文本数据后得到index_word,word_index,max_label_length
+def get_text_tokenizer(folder_path, text_data_path, num_examples):
+    max_label_length = 0
+    word_set = set()
+    with open(folder_path + "/" + text_data_path, "r") as f:
+        sentence_list = f.readlines()
+    for sentence in sentence_list[:num_examples]:
+        sen_word_list = preprocess_sentence(text_row_process(sentence)).split(" ")
+        max_label_length = max(max_label_length, len(sen_word_list))
+        word_set = word_set | set(sen_word_list)
+    
+    word_list = list(word_set)
+    index_word = dict(zip(range(1, len(word_list)+1), word_list))
+    word_index = dict(zip(word_list, range(1, len(word_list)+1)))
+    
+    return index_word, word_index, max_label_length
+
+# 对英文句子：小写化，切分句子，添加开始和结束标记，按单词切分
+def preprocess_sentence_en_word(s):
+    s = s.lower().strip()
     # 在单词与跟在其后的标点符号之间插入一个空格
     # 例如： "he is a boy." => "he is a boy ."
     # 参考：https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
@@ -37,28 +96,31 @@ def preprocess_en_sentence_word(s):
 
     # 除了 (a-z, A-Z, ".", "?", "!", ",")，将所有字符替换为空格
     s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
-
     s = s.strip()
-
+    """
     # 给句子加上开始和结束标记
     # 以便模型知道何时开始和结束预测
     s = '<start> ' + s + ' <end>'
+    """
     return s
 
 #对英文句子：小写化，切分句子，添加开始和结束标记，将空格转为<space>，按字符切分
-def preprocess_en_sentence_char(s):
+def preprocess_sentence_en_char(s):
     s = s.lower().strip()
+
     result = ""
     for i in s:
         if i == " ":
             result += "<space> "
         else:
-            result += i+" "
+            result += i + " "
+    """
     result = "<start> " + result.strip() + " <end>"
-    return result
+    """
+    return result.strip()
 
 # 对中文句子：按字切分句子，添加开始和结束标记
-def preprocess_ch_sentence(s):
+def preprocess_sentence_ch(s):
     s = s.lower().strip()
 
     s = [c for c in s]
@@ -66,28 +128,9 @@ def preprocess_ch_sentence(s):
     s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
 
     s = s.strip()
-
+    """
     # 给句子加上开始和结束标记
     # 以便模型知道何时开始和结束预测
     s = '<start> ' + s + ' <end>'
+    """
     return s
-
-#获取文本数据list
-def get_text_data(data_path, text_data_path, num_examples):
-    sentences_list = []
-    with open(data_path+"/" + text_data_path, "r") as f:
-        sen_list = f.readlines()
-    for sentence in sen_list[:num_examples]:
-        sentences_list.append(preprocess_sentence(text_row_process(sentence)))
-    return sentences_list
-
-#基于数据文本list构建tokenizer
-def tokenize(texts):
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='')  # 无过滤字符
-    tokenizer.fit_on_texts(texts)
-    sequences = tokenizer.texts_to_sequences(texts)  # 文本数字序列
-    sequences_length = []
-    for seq in sequences:
-        sequences_length.append([len(seq)])
-    sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding='post')
-    return sequences,sequences_length,tokenizer
