dc8757513c2e121970c507a3e6489a948edda67e
lxw <582773700@qq.com>
2020-11-11 22:23:42 +0800

    数据处理由dataset改为generator，并将参数配置写入json文件

---
+++ hlp/stt/las/data_processing/load_dataset.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 10:35:04 2020
+
+@author: 九童
+"""
+
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import json
+import tensorflow as tf
+from hlp.stt.las.config import config
+from hlp.stt.las.data_processing import librosa_mfcc, preprocess_text
+
+
+# 基于dataset中的audio_data_path_list和text_list来加载train或test数据
+def build_train_data(audio_data_path_list, text_list):
+    process_text_list = []
+    # 处理文本数据，格式变成如<start> z e r o <end>
+    for text in text_list:
+        process_text_list.append(preprocess_text.preprocess_en_sentence(text))
+    if config.if_is_first_train:
+        text_int_sequences, tokenizer = preprocess_text.tokenize(process_text_list)
+        # 获取音频和文本的最大length，从而进行数据补齐
+        max_input_length = get_max_audio_length(audio_data_path_list)
+        max_label_length = max_length(text_int_sequences)
+
+        # 若为初次训练，则将数据集的相关信息写入dataset_information.json文件
+        dataset_information_path = config.dataset_information_path
+
+        dataset_information = {}
+        dataset_information["vocab_tar_size"] = len(tokenizer.index_word) + 1
+        dataset_information["max_input_length"] = max_input_length
+        dataset_information["max_label_length"] = max_label_length
+        dataset_information["index_word"] = tokenizer.index_word
+        dataset_information["word_index"] = tokenizer.word_index
+
+        with open(dataset_information_path, 'w', encoding="utf-8") as f:
+            json.dump(dataset_information, f, ensure_ascii=False, indent=4)
+        configs = config.get_config_json(config.json_path)
+        # 将是否为初次训练改为false
+        config.set_config_json(configs, "train", "if_is_first_train", False)
+
+    else:
+        # 不是初次训练就基于初次训练时写入的word_index构建文本
+        dataset_information = get_dataset_information()
+        text_int_sequences = preprocess_text.get_text_int_sequences(process_text_list,
+                                                                    dataset_information["word_index"])
+    vocab_tar_size = dataset_information["vocab_tar_size"]
+    label_length_list = [[len(text_int)] for text_int in text_int_sequences]
+    return audio_data_path_list, text_int_sequences, label_length_list, vocab_tar_size
+
+
+# 加载number语料，返回语音文件list和对应文本字符串list
+def load_dataset_number(wav_path, label_path, num_examples=None):
+    # 获取number语料中训练集语音路径和文本的列表
+    text_data_path = label_path
+    files = os.listdir(wav_path)  # 得到数据文件夹下的所有文件名称list    
+    audio_path_list = files
+
+    # 获取语音路径list和文本list
+    audio_data_path_list = [wav_path + "\\" + audio_path for audio_path in audio_path_list[:num_examples]]
+    text_list = get_text_list(text_data_path)[:num_examples]
+    return audio_data_path_list, text_list
+
+
+# 创建一个 tf.data 数据集
+def create_dataset(path, path_to_file, num_examples, n_mfcc, batch_size):
+    input_tensor = librosa_mfcc.wav_to_mfcc(path, n_mfcc, num_examples)
+    target_tensor, targ_lang_tokenizer = preprocess_text.load_dataset(path_to_file, num_examples)
+
+    # 计算目标张量的最大长度 （max_length）
+    max_length_targ = preprocess_text.max_length(target_tensor)
+    max_length_inp = preprocess_text.max_length(input_tensor)
+
+    BUFFER_SIZE = len(input_tensor)
+    steps_per_epoch = len(input_tensor) // batch_size
+    dataset = tf.data.Dataset.from_tensor_slices((input_tensor, target_tensor)).shuffle(BUFFER_SIZE)
+    dataset = dataset.batch(batch_size, drop_remainder=True)
+    return steps_per_epoch, targ_lang_tokenizer, max_length_targ, max_length_inp, dataset
+
+
+# 加载数据
+def load_data(dataset_name, wav_path, label_path, train_or_test, num_examples):
+    # 基于某种语料获取其中语音路径和文本的list
+    if dataset_name == "number":
+        audio_data_path_list, text_list = load_dataset_number(wav_path, label_path, num_examples)
+
+    # 训练则对数据进行预处理，测试则直接返回
+    if train_or_test == "train":
+        return build_train_data(audio_data_path_list, text_list)
+    elif train_or_test == "test":
+        return audio_data_path_list, text_list
+
+
+# 读取文本文件，处理原始语料
+def get_text_list(text_path):
+    text_list = []
+    with open(text_path, "r") as f:
+        sentence_list = f.readlines()
+    for sentence in sentence_list:
+        text_list.append(sentence.strip().split("\t", 1)[1].lower())
+    return text_list
+
+
+def max_length(texts):
+    return max(len(t) for t in texts)
+
+
+# 获取最长的音频length(timesteps)
+def get_max_audio_length(audio_data_path_list):
+    max_audio_length = 0
+    for audio_path in audio_data_path_list:
+        audio_feature = librosa_mfcc.wav_to_mfcc(audio_path)
+        if (audio_feature):
+            max_audio_length = max(max_audio_length, len(audio_feature))
+
+    return max_audio_length
+
+
+# 获取预处理得到的语料集信息
+def get_dataset_information():
+    with open("./dataset_information.json", "r", encoding="utf-8") as f:
+        dataset_information = json.load(f)
+    return dataset_information
--- hlp/stt/las/train.py
+++ hlp/stt/las/train.py
@@ -9,27 +9,15 @@ formatted
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import time
 import tensorflow as tf
-from hlp.stt.las.data_processing import librosa_mfcc
-from hlp.stt.las.data_processing import preprocess_text
 from hlp.stt.las.model import las
+from hlp.stt.las.config import config
+from hlp.stt.las.data_processing import load_dataset
+from hlp.stt.las.data_processing.generator import data_generator
 
-
-# 创建一个 tf.data 数据集
-def create_dataset(path, path_to_file, num_examples, n_mfcc, batch_size):
-    input_tensor = librosa_mfcc.wav_to_mfcc(path, n_mfcc)
-    target_tensor, targ_lang_tokenizer = preprocess_text.load_dataset(path_to_file, num_examples)
-
-    # 计算目标张量的最大长度 （max_length）
-    max_length_targ = preprocess_text.max_length(target_tensor)
-    max_length_inp = preprocess_text.max_length(input_tensor)
-
-    BUFFER_SIZE = len(input_tensor)
-    steps_per_epoch = len(input_tensor) // batch_size
-    dataset = tf.data.Dataset.from_tensor_slices((input_tensor, target_tensor)).shuffle(BUFFER_SIZE)
-    dataset = dataset.batch(batch_size, drop_remainder=True)
-    return steps_per_epoch, targ_lang_tokenizer, max_length_targ, max_length_inp, dataset
 
 
 def loss_function(real, pred):
@@ -42,7 +30,7 @@ def loss_function(real, pred):
     return tf.reduce_mean(loss_)
 
 
-def train_step(inputx_1, targetx_2, enc_hidden, targ_tokenizer, model, las_optimizer, batch_size):
+def train_step(inputx_1, targetx_2, enc_hidden, word_index, model, las_optimizer, batch_size):
     loss = 0
 
     with tf.GradientTape() as tape:
@@ -50,7 +38,7 @@ def train_step(inputx_1, targetx_2, enc_hidden, targ_tokenizer, model, las_optim
         targetx_2 = tf.convert_to_tensor(targetx_2)
 
         # 解码器输入符号
-        dec_input = tf.expand_dims([targ_tokenizer.word_index['<start>']] * batch_size, 1)
+        dec_input = tf.expand_dims([word_index['<start>']] * batch_size, 1)
 
         predicted_result = ''
         target_result = ''
@@ -85,55 +73,71 @@ def train_step(inputx_1, targetx_2, enc_hidden, targ_tokenizer, model, las_optim
     variables = model.trainable_variables
     gradients = tape.gradient(loss, variables)  # 计算损失对参数的梯度
     las_optimizer.apply_gradients(zip(gradients, variables))  # 优化器反向传播更新参数
-    return batch_loss, las_optimizer
+    return batch_loss
 
 
 if __name__ == "__main__":
     # wav文件
-    path = ".\\data\\wav_train"
-
-    # 语音识别语料文件
-    path_to_file = ".\\data\\data_train.txt"
-
+    wav_path = config.train_wav_path
+    # 标签文件
+    label_path = config.train_label_path
     # 尝试实验不同大小的数据集
-    num_examples = 1928
-
+    num_examples = config.num_examples
     # 每一步mfcc所取得特征数
-    n_mfcc = 20
+    n_mfcc = config.n_mfcc
 
-    embedding_dim = 256
-    units = 512
-    batch_size = 64
-    steps_per_epoch, targ_lang_tokenizer, max_length_targ, max_length_inp, dataset = create_dataset(path,
-                                                                                                    path_to_file,
-                                                                                                    num_examples,
-                                                                                                    n_mfcc, batch_size)
-    vocab_tar_size = len(targ_lang_tokenizer.word_index) + 1  # 含填充的0
+    embedding_dim = config.embedding_dim
+    units = config.units
+    batch_size = config.train_batch_size
+    dataset_name = config.dataset_name
+    audio_feature_type = config.audio_feature_type
+
+    # 加载数据并预处理
+    train_data = load_dataset.load_data(dataset_name, wav_path,
+                                        label_path,
+                                        "train",
+                                        num_examples)
+    _, _, _, vocab_tar_size = train_data
+    batchs = len(train_data[0]) // batch_size
     optimizer = tf.keras.optimizers.Adam()
     model = las.las_model(vocab_tar_size, embedding_dim, units, batch_size)
 
+    # 获取训练语料信息
+    dataset_information = config.get_dataset_information()
+
+    # 构建数据生成器
+    train_data_generator = data_generator(
+        train_data,
+        "train",
+        batchs,
+        batch_size,
+        audio_feature_type,
+        dataset_information["max_input_length"],
+        dataset_information["max_label_length"]
+    )
     # 检查点
-    checkpoint_dir = './checkpoints'
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint_dir = config.checkpoint_dir
+    checkpoint_prefix = os.path.join(checkpoint_dir, config.checkpoint_prefix)
     checkpoint = tf.train.Checkpoint(optimizer=optimizer, model=model)
 
     # 恢复检查点目录 （checkpoint_dir） 中最新的检查点
     checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+    EPOCHS = config.epochs
 
-    EPOCHS = 2
+    word_index = dataset_information["word_index"]
 
     for epoch in range(EPOCHS):
         start = time.time()
         enc_hidden = model.initialize_hidden_state()
         total_loss = 0
         batch_start = time.time()
-        for (batch, (inp, targ)) in enumerate(dataset.take(steps_per_epoch)):
+        for batch, (inp, targ, target_length) in zip(range(1, batchs + 1), train_data_generator):
             x_1 = inp
             x_2 = targ
-            batch_loss, optimizer = train_step(x_1, x_2, enc_hidden, targ_lang_tokenizer,
-                                               model,
-                                               optimizer,
-                                               batch_size)  # 训练一个批次，返回批损失
+            batch_loss = train_step(x_1, x_2, enc_hidden, word_index,
+                                    model,
+                                    optimizer,
+                                    batch_size)  # 训练一个批次，返回批损失
             total_loss += batch_loss
 
             if (batch + 1) % 2 == 0:
@@ -142,9 +146,9 @@ if __name__ == "__main__":
                                                              batch_loss.numpy()))
                 print('Time taken for 2 batches {} sec\n'.format(time.time() - batch_start))
                 batch_start = time.time()
-        # 每 2 个周期（epoch），保存（检查点）一次模型
-        if (epoch + 1) % 2 == 0:
+        # 每 1 个周期（epoch），保存（检查点）一次模型
+        if (epoch + 1) % 1 == 0:
             checkpoint.save(file_prefix=checkpoint_prefix)
 
-        print('Epoch {} Loss {:.4f}'.format(epoch + 1, total_loss / steps_per_epoch))
+        print('Epoch {} Loss {:.4f}'.format(epoch + 1, total_loss / batchs))
         print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
