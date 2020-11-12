60bdb05b1211ebeae3d93803ca243ad82f5fb69b
lxw <582773700@qq.com>
2020-11-12 22:33:26 +0800

    去掉las中重复的metric，使用stt/utils中的metric。

--- hlp/stt/las/evaluate.py
+++ hlp/stt/las/evaluate.py
@@ -10,7 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from hlp.stt.las import train
 from hlp.stt.las.model import las
-from hlp.stt.las.metric import lers
+from hlp.stt.utils.metric import lers
 from hlp.stt.las.config import config
 from hlp.stt.las.data_processing import load_dataset
 from hlp.stt.las.data_processing.generator import data_generator
