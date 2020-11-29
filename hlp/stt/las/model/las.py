a6c9f9d0a83dfb477bcc7d8f0aa959e82265b10b
hlp-ai <hlp_ai@sina.com>
2020-11-29 10:00:17 +0800

    去掉无用括号；去掉Encoder.call方法中没有使用的参数。

--- hlp/stt/las/model/las.py
+++ hlp/stt/las/model/las.py
@@ -15,7 +15,7 @@ class pBLSTM(tf.keras.layers.Layer):
     def call(self, inputs):
         y = self.bidi_LSTM(inputs)
 
-        if (tf.shape(inputs)[1] % 2 == 1):
+        if tf.shape(inputs)[1] % 2 == 1:
             y = tf.keras.layers.ZeroPadding1D(padding=(0, 1))(y)
 
         y = tf.keras.layers.Reshape(target_shape=(-1, int(self.dim * 4)))(y)
@@ -33,11 +33,11 @@ class Encoder(tf.keras.Model):
         self.plstm2 = pBLSTM(self.dim // 2)
         self.plstm3 = pBLSTM(self.dim // 2)
 
-    def call(self, x, hidden):
+    def call(self, x):
         x = self.plstm1(x)
         x = self.plstm2(x)
         output = self.plstm3(x)
-        return output, hidden
+        return output
 
     def initialize_hidden_state(self):
         return tf.zeros((self.batch_sz, self.enc_units))
@@ -90,7 +90,7 @@ class las_model(tf.keras.Model):
         self.decoder = Decoder(vocab_tar_size, embedding_dim, units)
 
     def call(self, inputx_1, enc_hidden, dec_input):
-        enc_output, enc_hidden = self.encoder(inputx_1, enc_hidden)  # 前向计算，编码
+        enc_output, enc_hidden = self.encoder(inputx_1)  # 前向计算，编码
         dec_hidden = enc_hidden  # 编码器状态作为解码器初始状态？
         predictions, dec_hidden, _ = self.decoder(dec_input, dec_hidden, enc_output)
         return predictions, dec_hidden
