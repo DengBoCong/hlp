2498f1b68339cea068f65597fea7bb2018adf24c
hlp-ai <hlp_ai@sina.com>
2020-11-30 12:00:03 +0800

    添加需求包playsound;import改成相对导入；

--- hlp/tts/tacotron2/generator.py
+++ hlp/tts/tacotron2/generator.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
 
-from prepocesses import map_to_text, dataset_seq, dataset_mel
+from .prepocesses import map_to_text, dataset_seq, dataset_mel
+
 
 # train数据生成器
 def train_generator(wav_name_list, batch_size, csv_dir, tokenizer, wave_train_path, config):
@@ -10,16 +11,16 @@ def train_generator(wav_name_list, batch_size, csv_dir, tokenizer, wave_train_pa
         # 每epoch将所有数据进行一次shuffle
         order = np.random.choice(len(wav_name_list), len(wav_name_list), replace=False)
         audio_data_path_list = [wav_name_list[i] for i in order]
-        batchs = len(wav_name_list)//batch_size
+        batchs = len(wav_name_list) // batch_size
         for idx in range(batchs):
-            #逐步取音频名
+            # 逐步取音频名
             wav_name_list2 = audio_data_path_list[idx * batch_size: (idx + 1) * batch_size]
-            #根据文本取数据
+            # 根据文本取数据
             sentence_list = map_to_text(csv_dir, wav_name_list2)
             input_ids = dataset_seq(sentence_list, tokenizer, config)
             input_ids = tf.convert_to_tensor(input_ids)
 
-            #取音频数据
+            # 取音频数据
             wav_tensor, input_length = dataset_mel(
                 wave_train_path, config.max_input_length, wav_name_list2, config
             )
@@ -31,16 +32,16 @@ def train_generator(wav_name_list, batch_size, csv_dir, tokenizer, wave_train_pa
 def test_generator(wav_name_list, batch_size, csv_dir, tokenizer, wave_test_path, config):
     # generator只能进行一次生成，故需要while True来进行多个epoch的数据生成
     while True:
-        batchs = len(wav_name_list)//batch_size
+        batchs = len(wav_name_list) // batch_size
         for idx in range(batchs):
-            #逐步取音频名
+            # 逐步取音频名
             wav_name_list2 = wav_name_list[idx * batch_size: (idx + 1) * batch_size]
-            #根据文本取数据
+            # 根据文本取数据
             sentence_list = map_to_text(csv_dir, wav_name_list2)
             input_ids = dataset_seq(sentence_list, tokenizer, config)
             input_ids = tf.convert_to_tensor(input_ids)
 
-            #取音频数据
+            # 取音频数据
             wav_tensor, input_length = dataset_mel(
                 wave_test_path, config.max_input_length, wav_name_list2, config
             )
