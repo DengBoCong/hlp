b73a4a31b60701b84deeb5de4cc83206863afbfc
hlp-ai <hlp_ai@sina.com>
2020-12-09 13:49:25 +0800

    将两个数据集加载方法改成共用，去掉转写模式，用分割替代，增加数据集加载测试，对转写文本不切分。

--- hlp/stt/utils/load_dataset.py
+++ hlp/stt/utils/load_dataset.py
@@ -4,21 +4,50 @@ Description: 加载数据集, 获取音频路径list和转写文本list
 '''
 import os
 
-from text_process import get_text_list
-
 
 # 获得语音文件名和转写列表
-def load_data(dataset_name, data_path, text_row_style, num_examples):
+def load_data(dataset_name, data_path, num_examples):
     if dataset_name.lower() == "librispeech":
-        audio_data_path_list, text_list = _get_data_librispeech(data_path, text_row_style, num_examples)
+        audio_data_path_list, text_list = get_data_librispeech(data_path, num_examples)
     elif dataset_name.lower() == "thchs30":
-        audio_data_path_list, text_list = _get_data_thchs30(data_path, num_examples)
+        audio_data_path_list, text_list = get_data_thchs30(data_path, num_examples)
 
     return audio_data_path_list, text_list
 
 
-def _get_data_librispeech(data_path, text_row_style, num_examples=None):
-    # 获取librispeech数据文件下(train或test)所有的数据folder
+def get_text(line, colum_sep=" "):
+    """
+    MSG:
+        基于数据文本规则的行获取
+    Param:
+        line: 语料文件中每行索引及其对应文本
+        colum_sep: 可能的语音文件名和转写文本间的分隔符
+    Return:
+        音频对应的转写文本
+    """
+    if colum_sep is None:
+        return line.strip().lower()
+
+    return line.strip().split(colum_sep, 1)[1].lower()
+
+
+# 读取文本文件，并基于某种row_style来处理原始语料
+def get_text_list(text_path, colum_sep=" "):
+    text_list = []
+    with open(text_path, "r") as f:
+        sentence_list = f.readlines()
+    for sentence in sentence_list:
+        text_list.append(get_text(sentence, colum_sep))
+    return text_list
+
+
+def get_data_librispeech(data_path, num_examples=None):
+    """ 获得librispeech数据集的语音文件和转写列表
+
+        :param data_path: librispeech数据集路径
+        :param num_examples: 最大语音文件数
+        :return: (语音文件列表，转写列表)
+        """
     data_folder_list = []
     folders_first = os.listdir(data_path)
     for folder_first in folders_first:
@@ -29,7 +58,6 @@ def _get_data_librispeech(data_path, text_row_style, num_examples=None):
     audio_data_path_list = []
     text_list = []
 
-    # 基于每个数据文件夹进行语音路径和文本的获取
     for data_folder in data_folder_list:
         # 获取某个数据文件夹下所有文件路径
         files = os.listdir(data_folder)
@@ -42,18 +70,24 @@ def _get_data_librispeech(data_path, text_row_style, num_examples=None):
 
         # 获取语音路径list和文本list
         audio_data_path_list.extend([os.path.join(data_folder, audio_path) for audio_path in audio_path_list])
-        text_list.extend(get_text_list(os.path.join(data_folder, text_data_path), text_row_style))
+        text_list.extend(get_text_list(os.path.join(data_folder, text_data_path)))
 
     return audio_data_path_list[:num_examples], text_list[:num_examples]
 
 
-def _get_data_thchs30(data_path, num_examples=None):
-    # 语料文件夹路径
+def get_data_thchs30(data_path, num_examples=None):
+    """ 获得thchs数据集的语音文件和转写列表
+
+    :param data_path: thchs数据集路径
+    :param num_examples: 最大语音文件数
+    :return: (语音文件列表，转写列表)
+    """
     thchs30_dataset_dir = os.path.dirname(data_path)
 
     files = os.listdir(data_path)
     audio_data_path_list = []
     text_list = []
+    row_num = 1  # 第0行是汉字序列，1行是完整拼音序列，2是分离拼音序列
     for file in files:
         if os.path.splitext(file)[1] == ".wav":
             # 音频文件
@@ -63,11 +97,19 @@ def _get_data_thchs30(data_path, num_examples=None):
             # 对应的文本
             with open(os.path.join(thchs30_dataset_dir, "data", file + ".trn"), encoding='UTF-8') as fl:
                 txt_content = fl.readlines()
-            text = "".join(txt_content[0].strip().split(" "))
+            text = txt_content[row_num].strip()
             text_list.append(text)
 
     return audio_data_path_list[:num_examples], text_list[:num_examples]
 
 
 if __name__ == "__main__":
-    pass
+    dir_thchs30 = '../data/data_thchs30/train'
+    audio_fils, texts = get_data_thchs30(dir_thchs30)
+    print(audio_fils)
+    print(texts)
+
+    dir_librispeech = '../data/LibriSpeech/train-clean-5'
+    audio_fils, texts = get_data_librispeech(dir_librispeech)
+    print(audio_fils)
+    print(texts)
