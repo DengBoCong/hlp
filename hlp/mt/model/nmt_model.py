5827b8b511cac58b0046be695165364462799b58
hlp-ai <hlp_ai@sina.com>
2020-12-14 10:59:56 +0800

    格式化方法参数。

--- hlp/mt/model/nmt_model.py
+++ hlp/mt/model/nmt_model.py
@@ -11,8 +11,12 @@ from hlp.utils import optimizers as _optimizers
 
 def get_model(vocab_size_source, vocab_size_target):
     """获取模型"""
-    transformer = _transformer.Transformer(_config.num_layers, _config.d_model, _config.num_heads, _config.dff,
-                                           vocab_size_source + 1, vocab_size_target + 1,
+    transformer = _transformer.Transformer(_config.num_layers,
+                                           _config.d_model,
+                                           _config.num_heads,
+                                           _config.dff,
+                                           vocab_size_source + 1,
+                                           vocab_size_target + 1,
                                            pe_input=vocab_size_source + 1,
                                            pe_target=vocab_size_target + 1,
                                            rate=_config.dropout_rate)
