ba0b6a84ca9ed2e83d4cb59414bf6edb8de9232a
hlp-ai <hlp_ai@sina.com>
2020-12-19 12:06:49 +0800

    格式代码；补充注释；补充了输出信息。

--- hlp/stt/ds2/train.py
+++ hlp/stt/ds2/train.py
@@ -12,7 +12,7 @@ from hlp.stt.utils.load_dataset import load_data
 from hlp.stt.utils.text_process import split_and_encode
 
 
-def train_step(model, optimizer, input_tensor, target_tensor, input_length, target_length):
+def _train_step(model, optimizer, input_tensor, target_tensor, input_length, target_length):
     # 单次训练
     with tf.GradientTape() as tape:
         y_pred = model(input_tensor)
@@ -35,40 +35,38 @@ def train_step(model, optimizer, input_tensor, target_tensor, input_length, targ
 
 
 def train(model, optimizer,
-          train_data_generator, train_batchs, epochs,
-          valid_data_generator, valid_batchs, valid_epoch_freq,
+          train_data_generator, train_batches, epochs,
+          valid_data_generator, valid_batches, valid_epoch_freq,
           stop_early_limits, text_process_mode, index_word, manager, save_epoch_freq):
     # 构建history
     history = {"loss": [], "wers": [], "norm_lers": []}
 
-    # 迭代训练
     for epoch in range(1, epochs + 1):
         total_loss = 0
         print("Epoch %d/%d" % (epoch, epochs))
         epoch_start = time.time()
-        for batch, (input_tensor, target_tensor, input_length, target_length) in zip(range(1, train_batchs + 1),
+        for batch, (input_tensor, target_tensor, input_length, target_length) in zip(range(1, train_batches + 1),
                                                                                      train_data_generator):
-            print("Batch %d/%d" % (batch, train_batchs))
-
             batch_start = time.time()
-            batch_loss = train_step(model, optimizer, input_tensor, target_tensor, input_length, target_length)
+            batch_loss = _train_step(model, optimizer, input_tensor, target_tensor, input_length, target_length)
             total_loss += batch_loss
             batch_end = time.time()
 
-            # 打印批处理的信息
-            print("batch_time: %dms - batch_loss: %.4f" % ((batch_end - batch_start) * 1000, batch_loss))
+            print("Batch %d/%d batch_time: %dms - batch_loss: %.4f" % (
+                batch, train_batches, (batch_end - batch_start) * 1000, batch_loss))
+
         epoch_end = time.time()
-        # 打印epoch的信息
-        print("batchs: %d - epoch_time: %ds %dms/batch - loss: %.4f\n" % (
-            train_batchs, epoch_end - epoch_start, (epoch_end - epoch_start) * 1000 / train_batchs,
-            total_loss / train_batchs))
+        print("batches: %d - epoch_time: %ds - %dms/batch - loss: %.4f\n" % (
+            train_batches, epoch_end - epoch_start, (epoch_end - epoch_start) * 1000 / train_batches,
+            total_loss / train_batches))
 
         # 将损失写入history
-        history["loss"].append(total_loss / train_batchs)
+        history["loss"].append(total_loss / train_batches)
+
         # 验证并将相关指标写入history
         if epoch % valid_epoch_freq == 0 or epoch == epochs:
-            wers, norm_lers = compute_metric(model, valid_data_generator, valid_batchs, text_process_mode,
-                                                   index_word)
+            wers, norm_lers = compute_metric(model, valid_data_generator, valid_batches,
+                                             text_process_mode, index_word)
             history["wers"].append(wers)
             history["norm_lers"].append(norm_lers)
             print("平均WER:", wers)
@@ -87,7 +85,7 @@ def train(model, optimizer,
     return history
 
 
-def plot_history(history, valid_epoch_freq, history_img_path):
+def _plot_history(history, valid_epoch_freq, history_img_path):
     # 绘制loss
     plt.subplot(2, 1, 1)
     epoch1 = [i for i in range(1, 1 + len(history["loss"]))]
@@ -113,6 +111,7 @@ def plot_history(history, valid_epoch_freq, history_img_path):
 if __name__ == "__main__":
     # 获取训练配置和语料信息
     configs = get_config()
+    print("加载数据集信息...")
     dataset_info = get_dataset_info(configs["preprocess"]["dataset_info_path"])
 
     epochs = configs["train"]["train_epochs"]
@@ -121,16 +120,19 @@ if __name__ == "__main__":
     dataset_name = configs["preprocess"]["dataset_name"]
 
     # 加载训练数据
+    print("加载训练数据集...")
     train_audio_path_list, train_text_list = load_data(dataset_name, data_path, num_examples)
 
     valid_data_path = configs["valid"]["data_path"]
     # 是否含有验证valid数据集,若有则加载，若没有，则将train数据按比例切分一部分为valid数据
     if valid_data_path:
+        print("加载验证数据集...")
         valid_num_examples = configs["valid"]["num_examples"]
         valid_audio_data_path_list, valid_text_list = load_data(dataset_name,
                                                                 valid_data_path,
                                                                 valid_num_examples)
     else:
+        print("从训练集中划分验证数据...")
         valid_percent = configs["valid"]["valid_percent"]
         pos = ceil(len(train_audio_path_list) * valid_percent / 100)
         valid_audio_data_path_list, valid_text_list = train_audio_path_list[-pos:], train_text_list[-pos:]
@@ -188,6 +190,7 @@ if __name__ == "__main__":
                                          max_to_keep=configs["checkpoint"]['max_to_keep'])
     save_epoch_freq = configs["checkpoint"]["save_epoch_freq"]
     if manager.latest_checkpoint:
+        print("加载检查点...")
         checkpoint.restore(manager.latest_checkpoint)
 
     valid_epoch_freq = configs["valid"]["valid_epoch_freq"]
@@ -195,12 +198,17 @@ if __name__ == "__main__":
     index_word = dataset_info["index_word"]
 
     # 训练
-    history = train(model, optimizer, train_data_generator, train_batches, epochs, valid_data_generator, valid_batches,
-                    valid_epoch_freq, stop_early_limits, text_process_mode, index_word, manager, save_epoch_freq)
+    print("开始训练...")
+    history = train(model, optimizer,
+                    train_data_generator, train_batches, epochs,
+                    valid_data_generator, valid_batches, valid_epoch_freq,
+                    stop_early_limits, text_process_mode, index_word, manager, save_epoch_freq)
 
     # 绘制history并保存
     history_img_dir = configs["other"]["history_img_dir"]
     if not os.path.exists(os.path.dirname(history_img_dir)):
         os.makedirs(os.path.dirname(history_img_dir), exist_ok=True)
     history_img_path = history_img_dir + dataset_name + time.strftime("%Y%m%d%H%M%S", time.localtime()) + ".png"
-    plot_history(history, valid_epoch_freq, history_img_path)
+    _plot_history(history, valid_epoch_freq, history_img_path)
+
+    print("训练结束.")
