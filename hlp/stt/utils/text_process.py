4b40f20e8f425f62170a424ca7c9d30bf524ef7c
lxw <582773700@qq.com>
2020-12-21 16:21:59 +0800

    将stt.utils.text_process中切分代码用util.text_split中方法调用替换

--- hlp/stt/utils/text_process.py
+++ hlp/stt/utils/text_process.py
@@ -66,40 +66,56 @@ def split_sentences(sentences, mode):
 
 
 def _split_sentence_en_word(s):
+    """ 对英文文本小写并按词进行切分
+
+    :param s: 待切分的英文文本
+    :return: token列表
+    """
     s = s.lower().strip()
     # 在单词与跟在其后的标点符号之间插入一个空格
     # 例如： "he is a boy." => "he is a boy ."
     s = re.sub(r"([?.!,])", r" \1 ", s)  # 切分断句的标点符号
-    s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
-
-    # 除了 (a-z, A-Z, ".", "?", "!", ",")外的将所有字符替换为空格
-    s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
-    s = s.strip()
-    return s
-
-
-def _split_sentence_en_char(s):
-    s = s.lower().strip()
-
-    result = ""
-    for i in s:
-        if i == " ":
-            result += "<space> "
-        else:
-            result += i + " "
-    return result.strip()
-
-
-def _split_sentence_las_en_char(s):
-    s = s.lower().strip()
-    s = ' '.join(s)
-    s = re.sub(r"([?.!,])", r" \1 ", s)  # 切分断句的标点符号
-    s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
 
     # 除了 (a-z, A-Z, ".", "?", "!", ",")，将所有字符替换为空格
     s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
 
+    s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
+
+    return s.strip().split()
+
+
+def _split_sentence_en_char(s):
+    """ 对英文文本小写并按字符进行切分
+
+    :param s: 待切分的英文文本
+    :return: token列表
+    """
+    s = s.lower().strip()
+    s = re.sub(r"([?.!,])", r" \1 ", s)  # 切分断句的标点符号
+
+    # 除了 (a-z, A-Z, ".", "?", "!", ",")外的所有字符替换为空格
+    s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
+
+    s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
+
+    return [c for c in s.strip()]
+
+
+def _split_sentence_las_en_char(s):
+    """ 对英文文本小写并按字符进行切分
+
+    :param s: 待切分的英文文本
+    :return: token列表
+    """
+    s = s.lower().strip()
+    s = ' '.join(s)
+    s = re.sub(r"([?.!,])", r" \1 ", s)  # 切分断句的标点符号
+
+    # 除了 (a-z, A-Z, ".", "?", "!", ",")外的所有字符替换为空格
+    s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
+
     s = s.strip()
+    s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
 
     # 给句子加上开始和结束标记
     # 以便模型知道何时开始和结束预测
@@ -109,42 +125,63 @@ def _split_sentence_las_en_char(s):
 
 
 def _split_sentence_las_en_word(s):
+    """ 对英文文本小写并按词进行切分
+
+    :param s: 待切分的英文文本
+    :return: token列表
+    """
     s = s.lower().strip()
     # 在单词与跟在其后的标点符号之间插入一个空格
     # 例如： "he is a boy." => "he is a boy ."
     s = re.sub(r"([?.!,])", r" \1 ", s)  # 切分断句的标点符号
+
+    # 除了 (a-z, A-Z, ".", "?", "!", ",")，将所有字符替换为空格
+    s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
+
     s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
 
-    # 除了 (a-z, A-Z, ".", "?", "!", ",")外的将所有字符替换为空格
-    s = re.sub(r"[^a-zA-Z?.!,]+", " ", s)
-    s = s.strip()
     # 给句子加上开始和结束标记
     # 以便模型知道何时开始和结束预测
     s = '<start> ' + s + ' <end>'
+
+    s = s.strip().split()
+
     return s
 
 
 def _split_sentence_cn(s):
+    """ 对中文按字进行切分
+
+    :param s: 待切分的中文
+    :return: token列表
+    """
     s = s.lower().strip()
 
     s = [c for c in s]
     s = ' '.join(s)
     s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
-    s = s.strip()
 
-    return s
+    return s.strip().split()
 
 
 def _split_sentence_las_cn(s):
+    """ 对中文按字（或拼音按字母）进行切分
+
+    :param s: 待切分的中文
+    :return: token列表
+    """
     s = s.lower().strip()
 
     s = [c for c in s]
     s = ' '.join(s)
     s = re.sub(r'[" "]+', " ", s)  # 合并多个空格
-    s = s.strip()
+
     # 给句子加上开始和结束标记
     # 以便模型知道何时开始和结束预测
     s = '<start> ' + s + ' <end>'
+
+    s = s.strip().split()
+
     return s
 
 
